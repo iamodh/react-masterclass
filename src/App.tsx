768c12776a844c8f3d06a0e822f534dd1e7e022d
Donghwan Oh <imodh97@gmail.com>
2024-03-06 00:15:23 +0900

    #3.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("HELLO", value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="black" />
-      <Circle bgColor="tomato" text="HII" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 }
