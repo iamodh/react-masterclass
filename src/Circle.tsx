0c648758c26ee1072a6bb4ea39664412fad89eb2
Donghwan Oh <imodh97@gmail.com>
2024-03-05 23:52:08 +0900

    #3.4

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,14 +11,22 @@ const Container = styled.div<ContainerProps>`
   height: 100px;
   background-color: ${(props) => props.bgColor};
   border-radius: 50px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  const [counter, setCoutner] = useState(0);
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
