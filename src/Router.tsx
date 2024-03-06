7ffcfd555a376ff3979a8e209d5cac7f6d59547f
Donghwan Oh <imodh97@gmail.com>
2024-03-07 03:18:43 +0900

    #4.8

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,43 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./screens/Home";
+import { Route, Routes, createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
 import About from "./screens/About";
-function Router() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import Home from "./screens/Home";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
+import User from "./screens/users/User";
+import Followers from "./screens/users/Followers";
 
-export default Router;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+        errorElement: <ErrorComponent />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "users/:userId",
+        element: <User />,
+        children: [
+          {
+            path: "followers",
+            element: <Followers />,
+          },
+          {
+            path: "followings",
+            element: <Followers />,
+          },
+        ],
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
+
+export default router;
